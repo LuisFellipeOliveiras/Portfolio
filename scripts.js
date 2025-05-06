8157f8ba45b4e1f49b7557c9ab20bbfe3f8d0966
LUIS FELLIPE DE OLIVEIRA SOUSA <luislipe1920@gmail.com>
2025-05-06 11:41:14 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// Ativar o menu toggle (responsivo)
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.getElementById('nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
